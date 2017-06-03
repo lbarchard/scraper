47b177e4eef196830e647a382a59f72196c79cd1
Les Barchard <lbarchard@Less-MacBook-Pro.local>
2017-06-03 07:32:54 -0400

    fiddling with port #

--- server.js
+++ server.js
@@ -152,6 +152,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(8080, function() {
+  console.log("App running on port 8080!");
 });
