620514af9dd69bf695f029531fa5b2edc8a2f622
Les Barchard <lbarchard@lawn-143-215-119-176.lawn.gatech.edu>
2017-06-03 09:59:10 -0400

    fixed port log statement

--- server.js
+++ server.js
@@ -153,5 +153,5 @@ app.post("/articles/:id", function(req, res) {
 
 // Listen on port 3000
 app.listen(process.env.PORT || 3000, function() {
-  console.log("App running on port 8080!");
+  console.log("App running on port 3000!");
 });
